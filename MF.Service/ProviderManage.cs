a85994e4e542d1f838ef421ed9a34156ae0962e0
mohamedfrifita <74158390+mohamedfrifita@users.noreply.github.com>
2022-02-22 16:57:17 +0100

    Add files via upload

--- MF.Service/ProviderManage.cs
+++ MF.Service/ProviderManage.cs
@@ -13,6 +13,7 @@ namespace MF.Service
         {
             this.providers = providers;
         }
+        //Partie 2/Q13)a
        public IList<Provider>GetProvidersByName(string name)
         {
             var result = from p in providers
@@ -20,6 +21,7 @@ namespace MF.Service
                          select p;
             return result.ToList();
         }
+        //Partie 2/Q13)b
         public Provider GetFirstProvidersByName(string name)
         {
             var result = from p in providers
@@ -27,6 +29,7 @@ namespace MF.Service
                          select p;
             return result.FirstOrDefault();
         }
+        //Partie 2/Q13)c
         public Provider GetProvidersById(int id)
         {
             var result = from p in providers
